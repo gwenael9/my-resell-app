cebbf5aa7dd4f25126c349504ccfd2520dd28d1b
gwenael9 <gwenaelgueho@gmail.com>
2024-11-27 11:37:59 +0100

    page factures ok

--- frontend/src/views/FacturesView.vue
+++ frontend/src/views/FacturesView.vue
@@ -4,21 +4,32 @@
   />
   <LoadingComp v-if="loading" />
   <div v-if="factures" class="flex gap-2 justify-center flex-wrap">
+    <h2 class="text-xl font-semibold mb-4 text-left max-w-4xl w-full">
+      Toutes nos factures
+    </h2>
     <div
       v-for="facture in factures"
       :key="facture.id"
-      class="border w-[250px] p-2 rounded-md"
+      class="shadow-md border border-gray-200 w-[250px] p-4 rounded-lg hover:shadow-lg transition-shadow"
     >
-      <h3 class="font-semibold">Facture n°{{ facture.id }}</h3>
-      <p>{{ formatDates(facture.createdAt) }}</p>
-      <div class="flex justify-end">
-        <router-link :to="{ name: 'facture', params: { id: facture.id } }">
-          Voir
-        </router-link>
-      </div>
+      <router-link
+        :to="{ name: 'facture', params: { id: facture.id } }"
+        class="hover:text-primary transition-colors"
+      >
+        <h3 class="font-semibold text-lg mb-2">Facture #{{ facture.id }}</h3>
+        <p class="text-sm text-gray-500">
+          {{ formatDates(facture.createdAt) }}
+        </p>
+        <div class="flex justify-between items-center">
+          <p class="text-sm font-semibold m-0">
+            {{ facture.totalPriceTaxe }} €
+          </p>
+          <a-button><ArrowRight /></a-button>
+        </div>
+      </router-link>
     </div>
   </div>
-  <h2 class="text-center" v-if="!loading && !factures">
+  <h2 class="text-center" v-if="!loading && factures.length == 0">
     Aucune facture pour le moment
   </h2>
 </template>
@@ -30,8 +41,9 @@ import { formatDates } from "@/utils/formatDate";
 import { getFactures } from "@/api";
 import { Facture } from "@/types";
 import LoadingComp from "@/components/ui/LoadingComp.vue";
+import { ArrowRight } from "lucide-vue-next";
 
-const factures = ref<Facture[] | null>(null);
+const factures = ref<Facture[]>([]);
 const loading = ref(true);
 
 onMounted(async () => {
